2d963c9a4ae3def9b25465abd0aea2dff2c69cfe
Sarah Melanie White <sawrrawr@gmail.com>
2022-02-10 21:49:21 -0330

    reconfigure getAllProperties to interact with db with filter options

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -111,12 +111,53 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-  return pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
+  const queryParams = [];
+  let queryString = `SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties
+  JOIN property_reviews
+  ON properties.id = property_reviews.property_id
+  WHERE 1=1`;
+  // checking for filter params
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length}`;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `AND owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    const cost = options.minimum_price_per_night * 100;
+    queryParams.push(cost);
+    queryString += `AND cost_per_night >= $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    const cost = options.maximum_price_per_night * 100
+    queryParams.push(cost);
+    queryString += `AND cost_per_night <= $${queryParams.length}`;
+  }
+  queryString += `
+  GROUP BY properties.id `
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  }
+//finishing the queryString after all filters have been applied
+queryParams.push(limit);
+queryString += `
+ORDER BY cost_per_night
+LIMIT $${queryParams.length}`;
+
+  return pool.query(queryString, queryParams)
       .then((res) => {
         return res.rows;
       })
       .catch((err) => {
-        console.log(`ERROR: ${err.message}!`);
+        console.log(`ERROR: ${err}!`);
       })
 };
 
