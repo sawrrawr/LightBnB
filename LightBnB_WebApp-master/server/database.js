2d60ab8023459f90b060f318a9716b5da3a11989
Sarah Melanie White <sawrrawr@gmail.com>
2022-02-19 14:03:04 -0330

    ensure add property function works

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -169,10 +169,17 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+const addProperty = (property) => {
+  console.log(property);
+  const values = Object.values(property);
+
+  return pool.query(`
+  INSERT INTO properties (title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`, values)
+  .then((res) => {
+    return res.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
+};
 exports.addProperty = addProperty;
